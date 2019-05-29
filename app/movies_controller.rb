3daf732e2d3c6bfb4129087f57337bfc97181e51
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 17:21:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,6 +36,7 @@ def can_be_created_in_a_block(args = {title:"Home Alone", release_date:1990})
     m.lead = args[:lead],
     m.in_theaters = args[:in_theaters]
   end
+  binding.pry
 end
 
 def can_get_the_first_item_in_the_database
