67ccd35cd3b7696d8e35337f4633ca401fc20be1
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 16:49:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,11 @@ def can_be_created_in_a_block(args = __)
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    title
+    release_date
+    director
+    lead
+    in_theaters
   end
 end
 
