21e4ad8a7aae7d635fd7db3c2ff469eedd2536d8
Erin Stalzer <erinstalzer@gmail.com>
2019-05-29 16:59:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,11 +29,11 @@ def can_be_created_in_a_block(args = {title:"Home Alone", release_date:1990, dir
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = title
-    m.release_date = release_date
-    m.director = director
-    m.lead = lead
-    m.in_theaters = in_theaters
+    @title = title
+    @release_date = release_date
+    @director = director
+    @lead = lead
+    @in_theaters = in_theaters
   end
 end
 
